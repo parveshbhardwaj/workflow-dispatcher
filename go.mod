70698788cf07ed8ddd2b4eaada6a5181dcb8ca1e
Parvesh Bhardwaj <parvesh_bhardwaj@infosys.com>
2024-07-22 13:28:44 -0400

    draft versionn of workflow dispatch tester

---
+++ go.mod
@@ -0,0 +1,3 @@
+module WorkflowDispatcher
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// git hub repo details
+	owner := "parveshbhardwaj"
+	repo := "workflow-dispatcher"
+	workflowID := ""
+
+	// creating work flow input
+	workflowInput := struct {
+		Ref string
+	}{Ref: "main"}
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(workflowInput)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	// creating workflow dispatcher request
+	workflowDispatchURL := fmt.Sprintf("https://api.github.com/repos/%s/%s/actions/workflows/%s/dispatches",
+		owner, repo, workflowID)
+
+	ctx := context.Background()
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, workflowDispatchURL, &buf)
+
+	// setting header to the request
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("Authorization", "Bearer <YOUR-TOKEN>")
+	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+
+	if err != nil {
+		log.Printf("error building the request: %v", err)
+		return
+	}
+	// send the workflow dispatcher request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Printf("workflow triggered with status code %s", resp.Status)
+
+}
